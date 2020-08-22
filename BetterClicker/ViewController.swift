4990824c63a7e730e7bbe403916ef4b1404194a5
Virianium <ctan2280@gmail.com>
2020-08-22 11:26:13 +0800

    Initial Commit

---
+++ BetterClicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterClicker
+//
+//  Created by AcidOS on 22/8/20.
+//  Copyright © 2020 Swift Accelerator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
